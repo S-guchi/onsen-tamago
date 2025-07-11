f583ba36a01e301e0a3917f96e7717ff42382cd5
S-guchi <haraguchi.shoya@gmail.com>
2025-07-11 14:37:24 +0900

    [#1-1] スタート画面実装 - 温泉卵画像とStartボタン

---
+++ src/app/session/page.tsx
@@ -0,0 +1,7 @@
+export default function SessionPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl">セッション画面（準備中）</h1>
+    </div>
+  );
+}
